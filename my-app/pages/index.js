b80e71e1fb0e41d7439d49a27c43aef90dfd399e
wormholecowboy <wormholecowboy@gmail.com>
2022-05-08 21:09:53 -0400

    set up next and consts

---
+++ my-app/pages/index.js
@@ -0,0 +1,23 @@
+import { BigNumber, Contract, providers, utils } from 'ethers';
+import Head from 'next/head';
+import React, { useState, useEffect, useRef } from 'react';
+import Web3Modal from 'web3modal';
+import {
+  NFT_CONTRACT_ABI,
+  NFT_CONTRACT_ADDRESS,
+  TOKEN_CONTRACT_ABI,
+  TOKEN_CONTRACT_ADDRESS,
+} from '../constants';
+import styles from '../styles/Home.module.css';
+
+export default function Home() {
+  const zero = BigNumber.from(0);
+  const [walletConnected, setWalletConnected] = useState(false);
+  const [loading, setLoading] = useState(false);
+  const [tokensToBeClaimed, setTokensToBeClaimed] = useState(zero);
+  const [balanceOfCryptoDevTokens, setBalanceOfCryptoDevTokens] =
+    useState(zero);
+  const [tokenAmount, setTokenAmount] = useState(zero);
+  const [tokensMinted, setTokensMinted] = useState(zero);
+  const web3ModalRef = useRef();
+}
