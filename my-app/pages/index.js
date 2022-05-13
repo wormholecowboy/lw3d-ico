d101fdf1fb74ecaf614cc8f3cbd2a0d30625e32c
wormholecowboy <wormholecowboy@gmail.com>
2022-05-13 19:59:13 -0400

    all done, time to deploy

--- my-app/pages/index.js
+++ my-app/pages/index.js
@@ -181,7 +181,7 @@ export default function Home() {
       });
       connectWallet();
       getTotalTokensMinted();
-      getBalanceOfCryptoDevToken();
+      getBalanceOfCryptoDevTokens();
       getTokensToBeClaimed();
     }
   }, [walletConnected]);
@@ -228,4 +228,46 @@ export default function Home() {
       </div>
     );
   };
+
+  return (
+    <div>
+      <Head>
+        <title>Crypto Devs</title>
+        <meta name="description" content="ICO-Dapp" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <div className={styles.main}>
+        <div>
+          <div className={styles.title}>Welcome to Crypto Devs ICO!</div>
+          <div className={styles.description}>
+            You can claim or mint Crypto Dev tokens here
+          </div>
+          {walletConnected ? (
+            <div>
+              <div className={styles.description}>
+                You have minted {utils.formatEther(balanceOfCryptoDevTokens)}{' '}
+                Crypto Dev Tokens
+              </div>
+              <div className={styles.description}>
+                Overall {utils.formatEther(tokensMinted_)}/10000 have been
+                minted!!!
+              </div>
+              {renderButton()}
+            </div>
+          ) : (
+            <button onClick={connectWallet} className={styles.button}>
+              Connect your wallet
+            </button>
+          )}
+        </div>
+        <div>
+          <img className={styles.image} scr="./0.svg" />
+        </div>
+      </div>
+
+      <footer className={styles.footer}>
+        Made with &#10084; by Crypto Devs
+      </footer>
+    </div>
+  );
 }
